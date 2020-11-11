8e62d4db4e8f0d3ccdd57b002d9864940dc2d2fa
Udit Pathak <pathakprintudit@gmail.com>
2020-11-11 15:43:20 +0530

    User Model Created

---
+++ models/user.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const uuidv4 = require("uuid");
+const crypto = require("crypto")
+const Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    _id : mongoose.ObjectId,
+    name : {
+        type: String,
+        required : true,
+        trim : true
+    },
+    email : {
+        type: String,
+        required : true,
+        trim : true,
+        unique : true
+    },
+    salt : String,
+    encryp_password : {
+        type : String ,
+        required : true
+    },
+    // TODO: After the order and product schema is made
+    purchases : {
+        type : Array,
+        default : []
+    },
+    cart : {
+        type : Array,
+        default : []
+    },
+    role : {
+        type : Number,
+        default : 0
+    }
+});
+
+userSchema.virtual("password")
+        .set(function(password){
+            this._password = password;
+            this.salt = uuidv4();
+            this.encryp_password = this.securePassword(password)
+        })
+        .get(function(){
+            return this._password;
+        });
+
+userSchema.method = {
+
+    authenticate : function(plainpassword){
+        return this.securePassword(plainpassword) === this.encryp_password
+    },
+    securePassword : function(plainPassword){
+
+        if(!plainPassword)
+            return "";
+
+        try{
+            return crypto.createHmac("sha256",this.salt)
+                .update(plainPassword)
+                .digest("hex")
+        }catch(err){
+            return "Error"
+        }
+        
+    }
+}
+
+module.exports = mongoose.model("User",userSchema);
