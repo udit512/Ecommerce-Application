11e43ae7c150a05e431e883ca902a2efca515b15
Udit Pathak <pathakprintudit@gmail.com>
2020-11-12 07:17:59 +0530

    Order Model Created

--- models/order.js
+++ models/order.js
@@ -1,3 +1,32 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Product = require("./product");
 
+var productCartSchema = new Schema({
+    product : {
+        type :Schema.Types.ObjectId,
+        ref : "Product"
+    },
+    count : Number,
+    price : Number
+});
+
+var orderSchema = new Schema({
+    _id : Schema.Types.ObjectId,
+    transaction_id : String,
+    products : [productCartSchema],
+    //TODO: SET THE ENUM FOR ORDER STATUS
+    order_status : {
+        type : String,
+        default : "Processing",
+        enum : ["Pending"]
+    },
+    order_date : Date,
+    update : Date,
+    order_address : String,
+    user : {
+        type : Schema.Types.ObjectId,
+        ref : "User"
+    },
+    order_amount : Number
+});
